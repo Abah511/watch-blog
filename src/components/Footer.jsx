a07b260c046e5fb608392116599bc1050fc6f1aa
Abah511 <ab.ah5113306@gmail.com>
2025-04-10 19:41:17 +0500

    more addition
    banner added to home screen and footer edited

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
+// src/components/Footer.jsx
+import './Footer.css';
+import { Link } from 'react-router-dom';
+
 const Footer = () => (
-    <footer>
-      <p>© 2025 Watch Blog. All rights reserved.</p>
-    </footer>
-  );
-  
-  export default Footer;
-  
+  <footer className="footer">
+    <div className="footer-links">
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/faqs">FAQs</Link>
+      <Link to="/privacy">Privacy Policy</Link>
+      <Link to="/terms">Terms & Conditions</Link>
+    </div>
+    <p className="footer-text">© 2025 Watch Blog. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
-import WatchSlider from '../components/WatchSlider';
+import WatchSlider from "../components/WatchSlider";
+import PromoBanner from "../components/PromoBanner";
 
 const Home = () => (
   <div>
     <h1>Welcome to the New Watch Blog</h1>
     <p>Explore top brands, blogs, and more.</p>
+
     <WatchSlider />
+    <PromoBanner />
   </div>
 );
 
