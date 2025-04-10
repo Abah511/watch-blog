cd928868008c6bc03211b6e22d61ffc308872419
Abah511 <ab.ah5113306@gmail.com>
2025-04-10 10:41:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import blogs from './data/blogs';  // Corrected import
+import Blogs from './data/blogs';  // Corrected import
 import BlogDetail from './pages/BlogDetail';
 import Inventory from './pages/Inventory';
 import Contact from './pages/contact';
@@ -19,7 +19,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<blogs />} />
+        <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:id" element={<BlogDetail />} />
         <Route path="/inventory" element={<Inventory />} />
         <Route path="/contact" element={<Contact />} />
