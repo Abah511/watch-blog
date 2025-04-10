ed8a8b9d078cda162ee93e88219bdb9a0a03feed
Abah511 <ab.ah5113306@gmail.com>
2025-04-10 12:25:32 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => (
     <div>
-      <h1>Welcome to the Watch Blog</h1>
+      <h1>Welcome to the New Watch Blog</h1>
       <p>Explore top brands, blogs, and more.</p>
     </div>
   );
